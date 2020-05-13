8fd633e371f48f035dac0e77bd9c3e8222eecaee
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2020-05-12 21:40:46 -0700

    generateQuestion

--- TriviaGame/src/Maze/Door.java
+++ TriviaGame/src/Maze/Door.java
@@ -1,8 +1,10 @@
 package Maze;
 
+import java.util.Scanner;
+
 public class Door {
 	private boolean closed = false;
-	private Question question;
+	private Question question = generateQuestion();
 	
 	public boolean isClosed() {
 		return this.closed;
@@ -15,8 +17,19 @@ public class Door {
 		this.closed = true;
 	}
 	
+	private Question generateQuestion() {
+		return question;
+	}
+	
 	public Question getQuestion() {
 		return this.question;
 	}
 	
+	public boolean answerQuestion() {
+		Scanner sc = new Scanner(System.in);
+		this.question.PromptQuestion();
+		String answer = sc.next();
+		sc.close();
+		return this.question.CheckAnswer(answer);
+	}
 }
